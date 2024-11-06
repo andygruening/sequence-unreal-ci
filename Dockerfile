f18f0bae3fae8fe23dc56d407a5e545f5fc3e659
Andy Grüning <andygruening@gmail.com>
2024-11-06 15:34:28 +0100

    updates

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM ghcr.io/epicgames/unreal-engine:dev-slim-5.4.3
 
 WORKDIR /
 
-COPY . /
+COPY . /SequencePlugin/
 
 USER root
-RUN chmod +x ./build.sh
+RUN chmod +x ./SequencePlugin/build.sh
 
-CMD ["./build.sh"]
+CMD ["./SequencePlugin/build.sh"]
--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-/home/ue4/UnrealEngine/Engine/Build/BatchFiles/RunUAT.sh \
-	BuildCookRun \
-	-utf8output \
-	-platform=Linux \
-	-clientconfig=Shipping \
-	-serverconfig=Shipping \
-	-project=/SequenceUnreal.uproject \
-	-noP4 -nodebuginfo -allmaps \
-	-cook -build -stage -prereqs -pak -archive \
-	-archivedirectory=/Packaged
+/home/ue4/UnrealEngine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun -project=/SequencePlugin/SequenceUnreal.uproject -nop4 -clientconfig=Development -serverconfig=Development -unrealexe=/home/ue4/UnrealEngine/Engine/Binaries/Win64/UnrealEditor-Cmd.exe -utf8output -platform=Linux -targetplatform=Linux -build -cook -map=Default.umap -cookdir=/Packaged/ -skipcookingeditorcontent -unversionedcookedcontent -pak -distribution -compressed -stage -package -stagingdirectory="%~dp0Dist"
+
